24cb6a6d842c20574eb3fa2b1d534a3270f5a245
Yankovsky Andrey < @WB-1080.corp.wildberries.ru>
2013-01-26 20:02:57 +0400

    Its alive

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
+window.document.addEventListener("mousemove", function(event) {
+/*    myClientX = event.clientX;
+    myClientY = event.clientY;*/
+}, false);
+
 window.onload = function() {
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
 
-    var gameEnd = false;
+    var player = {x : 100, y : 100};
 
-    while (!gameEnd) {
+    function updateGameCycle() {
         update();
         draw()
     }
-}
+
+    var interval = setInterval(updateGameCycle, 100);
+
+    function gameEnd() {
+        clearInterval(interval);
+    }
+
+    function update() {
+        player.x += 5;
+    }
+
+    function draw() {
+        ctx.fillStyle = "orange";
+        ctx.fillRect(0, 0, 600, 600);
+
+        ctx.fillStyle = "black";
+        ctx.fillRect(player.x, player.y, 100, 100);
+    }
+};
